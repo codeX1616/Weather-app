1ca2b4a8d2ac19119cb7bffa37fbec83e4b4aac1
System Administrator <pjoshi@mphrx.com>
2023-12-10 00:05:14 +0530

    codeX1616-Adding humidity and wind elements

--- weather-app/src/components/Weather-app/WeatherApp.tsx
+++ weather-app/src/components/Weather-app/WeatherApp.tsx
@@ -17,6 +17,27 @@ const WeatherApp = () => {
                     <img src={search_icon} alt=' ' />
                 </div>
             </div>
+            <div className="weather-image">
+                <img src={cloud_icon} alt="" />
+            </div>
+            <div className="weather-temp">24°C</div>
+            <div className="weather-location">London</div>
+            <div className="date-container">
+                <div className="element">
+                    <img src={humidity_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="humidity-percent">64%</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+                <div className="element">
+                    <img src={wind_icon} alt="" className="icon"/>
+                    <div className="data">
+                        <div className="wind-speed">18 km/hr</div>
+                        <div className="text">Humidity</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
